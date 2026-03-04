6d048d9f4fdc05a85e9494972a63aa57819afdcd
Noman Khan <nomankhan07686@gmail.com>
2026-03-04 09:16:10 +0530

    initial commit

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  plugins: [react()],
+  base: '/<your-repo-name>/', // <-- ADD THIS (replace with your exact GitHub repository name)
 })
